83dde1ca667d80d28f99dcc4a4eb52dff6dfbfd7
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-10-19 12:57:50 +0300

    Challenge 1: Profile Card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="ana.jpg" alt="Orășanu Ana" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Orășanu Ana-Maria</h1>
+      <h3>Junior Full Stack Developer</h3>
+      <p>
+        I am a Junior Full Stack Developer and a first-year master's student. I
+        enjoy designing web applications and solving problems through
+        technology. In my free time, I love reading fantasy and psychology
+        books, watching YouTube, and taking walks outside.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML" emoji="♟️" color="orange" />
+      <Skill skill="JavaScript" emoji="🪄" color="yellow" />
+      <Skill skill="React" emoji="⚛️" color="lightblue" />
+      <Skill skill="jQuery" emoji="✨" color="lightblue" />
+      <Skill skill="Node.js" emoji="💎" color="lightgreen" />
+      <Skill skill="Express" emoji="🚅" color="lightgrey" />
+      <Skill skill="CSS" emoji="💄" color="lightblue" />
+      <Skill skill="Bootstrap" emoji="👒" color="purple" />
+      <Skill skill="MongoDB" emoji="🖼️" color="lightgreen" />
+      <Skill skill="SQL" emoji="🎁" color="orange" />
+      <Skill skill="C++" emoji="👑" color="lightblue" />
+      <Skill skill="PHP" emoji="🧸" color="purple" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
